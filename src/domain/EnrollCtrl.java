e22f922752dfd212c2eb4f16c5f7b4650884b131
Mohammad Salamati <salamati9819@gmail.com>
2021-06-25 21:41:49 +0430

    Extract checker methods from for body

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -18,8 +18,11 @@ public class EnrollCtrl {
                         throw new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
                 }
             }
+        }
+        for (Offering o : offerings) {
             List<Course> prereqs = o.getCourse().getPrerequisites();
-            nextPre://continue
+            nextPre:
+//continue
             for (Course pre : prereqs) {
 
                 for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
@@ -30,21 +33,37 @@ public class EnrollCtrl {
                 }
                 throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
             }
-            for (Offering o2 : offerings) {
-                if (o == o2)
-                    continue;
-                //TODO method extraction
-                if (o.getExamTime().equals(o2.getExamTime()))
-                    throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time", o, o2));
-                if (o.getCourse().equals(o2.getCourse()))
-                    throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
-            }
         }
-
+        checkForExamTimeConflict(offerings);
+        checkForDuplicateEnrollRequest(offerings);
         checkForGPALimit(student, offerings);
 
         for (Offering o : offerings)
             student.takeCourse(o.getCourse(), o.getSection());
+    }
+
+    private void checkForExamTimeConflict(List<Offering> offerings) throws EnrollmentRulesViolationException {
+        for (Offering o : offerings) {
+            for (Offering o2 : offerings) {
+                if (o == o2)
+                    continue;
+
+                if (o.getExamTime().equals(o2.getExamTime()))
+                    throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time", o, o2));
+            }
+        }
+    }
+
+    private void checkForDuplicateEnrollRequest(List<Offering> offerings) throws EnrollmentRulesViolationException {
+        for (Offering o : offerings) {
+            for (Offering o2 : offerings) {
+                if (o == o2)
+                    continue;
+
+                if (o.getCourse().equals(o2.getCourse()))
+                    throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
+            }
+        }
     }
 
     private void checkForGPALimit(Student student, List<Offering> offerings) throws EnrollmentRulesViolationException {
