48cb3902af51dedafd728b0ad44c428abea59827
Mohammad Salamati <salamati9819@gmail.com>
2021-06-25 21:30:48 +0430

    Extract checkForGPALimit method

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -7,29 +7,29 @@ import domain.exceptions.EnrollmentRulesViolationException;
 
 public class EnrollCtrl {
     //TODO  Could have better variable names
-	public void enroll(Student student, List<CSE> courses) throws EnrollmentRulesViolationException {
+    public void enroll(Student student, List<CSE> courses) throws EnrollmentRulesViolationException {
         Map<Term, Map<Course, Double>> transcript = student.getTranscript();
 
-		for (CSE o : courses) {
-		    //TODO method extraction checkIsPassed
+        for (CSE o : courses) {
+            //TODO method extraction checkIsPassed
             for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
                 for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
                     if (r.getKey().equals(o.getCourse()) && r.getValue() >= 10)
                         throw new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
                 }
             }
-			List<Course> prereqs = o.getCourse().getPrerequisites();
-			nextPre://continue
-			for (Course pre : prereqs) {
+            List<Course> prereqs = o.getCourse().getPrerequisites();
+            nextPre://continue
+            for (Course pre : prereqs) {
 
                 for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
                     for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
                         if (r.getKey().equals(pre) && r.getValue() >= 10)//TODO not the condition and replace continue with exception
                             continue nextPre;
                     }
-				}
-				throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
-			}
+                }
+                throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
+            }
             for (CSE o2 : courses) {
                 if (o == o2)
                     continue;
@@ -39,18 +39,20 @@ public class EnrollCtrl {
                 if (o.getCourse().equals(o2.getCourse()))
                     throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
             }
-		}
+        }
 
-		int unitsRequested = courses.stream().mapToInt(o -> o.getCourse().getUnits()).sum();
+        checkForGPALimit(student, courses);
 
-        double gpa = student.getGpa();
+        for (CSE o : courses)
+            student.takeCourse(o.getCourse(), o.getSection());
+    }
 
-        //TODO hasPassedMinimumGPAUnits
-		if ((gpa < 12 && unitsRequested > 14) ||
-				(gpa < 16 && unitsRequested > 16) ||
-				(unitsRequested > 20))
-			throw new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
-		for (CSE o : courses)
-			student.takeCourse(o.getCourse(), o.getSection());
-	}
+    private void checkForGPALimit(Student student, List<CSE> courses) throws EnrollmentRulesViolationException {
+        int unitsRequested = courses.stream().mapToInt(o -> o.getCourse().getUnits()).sum();
+
+        if ((student.getGpa() < 12 && unitsRequested > 14) ||
+                (student.getGpa() < 16 && unitsRequested > 16) ||
+                (unitsRequested > 20))
+            throw new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, student.getGpa()));
+    }
 }
